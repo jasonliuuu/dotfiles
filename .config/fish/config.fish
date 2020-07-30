96b0aa11cd82fedee7080622fab35f01a509e1e7
jasonliuuu <oiabot2003@outlook.com>
2020-07-30 21:43:34 +0800

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,19 @@
+PF_ASCII=arch pfetch
 alias ls="lsd"
+alias vim="nvim"
+alias open="nautilus"
+alias whatsmyip="curl -L tool.lu/ip"
+alias puu="sudo pacman -Syu"
+alias reload="source $HOME/.config/fish/config.fish && echo '>> OH MY, FISH configurations are reloaded!'"
+alias colortest="curl -s https://gist.githubusercontent.com/HaleTom/89ffe32783f89f403bba96bd7bcd1263/raw/ | bash"
+alias tweakfish="nvim ~/.config/fish/config.fish"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/jason/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
-starship init fish | source
+# starship init fish | source
 
 set fish_greeting
-pfetch 
+ 
 echo " 📟 Welcome to Manjaro Linux x86_64!"
